663a09df00f8fef3dc69644fff1e5dc186b2f35b
nikit.cpp <nikit.cpp@yandex.ru>
2015-04-15 01:22:09 +0300

    extraction for all

--- sikulix2api/src/main/java/my/superpackage/LibLoader.java
+++ sikulix2api/src/main/java/my/superpackage/LibLoader.java
@@ -7,8 +7,13 @@ import org.apache.commons.lang3.SystemUtils;
 import org.sikuli.basics.FileManager;
 import org.sikuli.basics.Settings;
 
+import com.melloware.jintellitype.JIntellitype;
+
 public class LibLoader {
 	public static void main(String... args) {
+	}
+	
+	public static void extractLib() throws LoadLibException {
 		String tempFolder = System.getProperty("java.io.tmpdir");
 		System.out.println(tempFolder);
 		
@@ -18,8 +23,12 @@ public class LibLoader {
 		if(SystemUtils.IS_OS_WINDOWS){
 			if(SystemUtils.OS_ARCH.equals("amd64")){
 				// load windows 64 JIntellitype
+				target = new File(tempFolder, "JIntellitype64.dll");
+				resource = "lib\\windows\\JIntellitype64.dll";
 			}else if(SystemUtils.OS_ARCH.equals("x86")){
 				// load windows 32 JIntellitype
+				target = new File(tempFolder, "JIntellitype.dll");
+				resource = "lib\\windows\\JIntellitype.dll";
 			}
 		} else if(SystemUtils.IS_OS_LINUX){
 			if(SystemUtils.OS_ARCH.equals("amd64")){
@@ -28,6 +37,8 @@ public class LibLoader {
 				resource = "lib/linux/libJXGrabKey-64.so";
 			}else if(SystemUtils.OS_ARCH.equals("x86")){
 				// load linux 32 JIntellitype
+				target = new File(tempFolder, "libJXGrabKey-32.so");
+				resource = "lib/linux/libJXGrabKey-32.so";
 			}
 		}
 		
@@ -41,5 +52,8 @@ public class LibLoader {
 		boolean extracted = FileManager.extractResource(resource, target);
 		System.out.println("Resource extracted? " + extracted);
 		
+		if(!extracted){
+			throw new LoadLibException("Lib are not extracted");
+		}
 	}
 }
